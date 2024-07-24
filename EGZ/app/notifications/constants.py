f5e436e4f430304a761e5476f95ee47f254e06b6
EdwinML <edwinml48@gmail.com>
2024-07-23 23:40:12 -0500

    fix/varios

--- EGZ/app/notifications/constants.py
+++ EGZ/app/notifications/constants.py
@@ -13,7 +13,7 @@ class TextToSend:
 
     def enrollment(tournament:Tournaments, name: str):
         text = f'''Hola *{name}* 😁, estas inscrito al torneo *{tournament.name}* con fecha de inicio *{tournament.start_date}*. 
-                \nPodras realizar tus jugadas en el siguiente enlace:\n\n{URL_FRONT}/tournament/{tournament.id}'''
+                \nPodras realizar tus jugadas en el siguiente enlace:\n\n{URL_FRONT}tournament/{tournament.id}'''
         return text
     
     def declining(tournament:Tournaments, name: str):
@@ -25,20 +25,20 @@ class TextToSend:
     
     def eliminated(tournament:Tournaments, name: str, fase: str):
         text = f''' Hola *{name}* 😖, fuiste eliminado del torneo *{tournament.name}* en la fase de *{fase}*.
-                \n✅ Puedes revisar los resultados  en {URL_FRONT}/tournament/{tournament.id}
+                \n✅ Puedes revisar los resultados  en {URL_FRONT}tournament/{tournament.id}
                 \n✅ Puedes probar suerte en otros torneos y ganar 💰💰💰 en {URL_FRONT}/tournaments
                 '''
         return text
     
     def stage_passed(tournament:Tournaments, name: str, fase: str):
         text = f''' Hola *{name}* 😁.Felicidades!!🎉, estas un paso mas cerca del premio 💸💰💵,superaste la fase de *{fase}* del torneo *{tournament.name}*.
-                \n✅ Puedes revisar los resultados  en {URL_FRONT}/tournament/{tournament.id}\n  
+                \n✅ Puedes revisar los resultados  en {URL_FRONT}tournament/{tournament.id}\n  
                 '''
         return text
 
     def user_not_play_games(tournament:Tournaments, name: str, stage: str):
         text = f''' Hola *{name}* 😣. Observamos que no completaste tus jugadas en la fase de *{stage}* del torneo *{tournament.name}*. No te pierdas la oportunidad de ganar 💸💰💵.
-                \n✅ Realiza tus jugadas en {URL_FRONT}/tournament/{tournament.id}\n
+                \n✅ Realiza tus jugadas en {URL_FRONT}tournament/{tournament.id}\n
                 '''
         return text
 
@@ -51,13 +51,13 @@ class TextToSend:
     def user_equal_poitns(tournament:Tournaments, name1: str, name2:str,list_date_1, list_date_2, stage: str):
         text = f''' Hola *{name1}* 👀. Empataste con el usuario *{name2}* en la fase de *{stage}* del torneo *{tournament.name}*. El desempate se decide por quien fue el primero en completar sus resultado por emfrentamiento.
                 \n✅ (*{name1}*){list_date_1[0][0].strftime('%d/%m/%Y %H:%M')} - {list_date_2[0][0].strftime('%d/%m/%Y %H:%M')}(*{name2}*)
-                \n✅ Para mas detalle revisa el torneo : {URL_FRONT}/tournament/{tournament.id}
+                \n✅ Para mas detalle revisa el torneo : {URL_FRONT}tournament/{tournament.id}
                 '''
         return text
     
     def user_has_not_played(footballgame:FootballGames, name: str):
         text = f''' Hola *{name}* 👀. Te recordamos que falta menos de *1 hora* ⏰ para que inicie el partido de *{footballgame.home_team} - {footballgame.away_team}* correspondiente a la *{footballgame.tournament_stage}* del torneo *{footballgame.tournament.name}*.   
-                \n✅ Recuerda *realizar tu jugada* antes del comienzo del partido ⏳ en {URL_FRONT}/tournament/{footballgame.tournament_id}\n
+                \n✅ Recuerda *realizar tu jugada* antes del comienzo del partido ⏳ en {URL_FRONT}tournament/{footballgame.tournament_id}\n
                 '''
         return text
   
