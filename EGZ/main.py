ffbe72628adf0059699669083ef60a89dfaf1459
EdwinML <edwinml48@gmail.com>
2024-07-31 03:32:02 -0500

    fix/schedule_timezone

--- EGZ/main.py
+++ EGZ/main.py
@@ -13,6 +13,7 @@ from app.competitions.schedule import cron_job_adding_match
 from app.notifications.schedule import cron_job_notifications
 from app.tournaments.schedule import cron_job_start_tournament, cron_job_update_footballgames
 from apscheduler.schedulers.background import BackgroundScheduler
+import pytz
 
 # Creacion de la BD
 Base.metadata.create_all(bind=engine)
@@ -44,9 +45,9 @@ handler = Mangum(app)
 
 @app.on_event('startup')
 def init_data():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(cron_job_notifications, 'cron', minute='0')
+    scheduler = BackgroundScheduler(timezone=pytz.timezone("America/Lima"))
+    scheduler.add_job(cron_job_notifications, 'cron', minute=2)
     scheduler.add_job(cron_job_start_tournament, 'cron', hour=0, minute=0)
-    scheduler.add_job(cron_job_update_footballgames, 'cron', minute='5')
+    scheduler.add_job(cron_job_update_footballgames, 'cron', minute=5)
     scheduler.add_job(cron_job_adding_match, 'cron', day_of_week='thu', hour=11, minute=0)
     scheduler.start()
