78973515045a2868dd745f783324a9dc94904838
eva.lucas.salvador <eva.lucas.salvador@gmail.com>
2024-08-20 19:00:34 -0500

    fix/schedule_add_match

--- EGZ/app/competitions/utils.py
+++ EGZ/app/competitions/utils.py
@@ -18,6 +18,7 @@ def preferred_random_values(matchs: List[Matchs] , len_loop: int):
         if len_loop <= len(list_match_peru):
             matchs_random = random.sample( list_match_peru, len_loop)
         else:
+            print("len_loop - len(list_match_peru) : ", len_loop - len(list_match_peru))
             matchs_random = random.sample( list_match_peru, len_loop) + random.sample( list_match_otros, len_loop - len(list_match_peru))
     else:
         matchs_random = random.sample( list_match_otros, len_loop)
